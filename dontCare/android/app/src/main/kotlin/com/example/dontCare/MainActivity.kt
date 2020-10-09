b777bb092b5cd50cfe09acb96759ddde2eed7c4c
sawyer816 <sawyer816@gmail.com>
2020-10-09 13:55:36 -0700

    Deleted Unused Files

--- dontCare/android/app/src/main/kotlin/com/example/dontCare/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dontCare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- new_hello/android/app/src/main/java/com/example/new_hello/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.new_hello;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
